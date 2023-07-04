89234272fd70f77e0fb76c8b291f91daff4125f5
dhalsdl12 <dhalsdl12@gmail.com>
2023-07-04 23:28:09 +0900

    award

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -3,27 +3,31 @@ import Introduce from "./Introduce";
 
 import { DataProps } from "@/types";
 
+
 const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <h1 className="leading-[1.15]">
-          {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          안녕하세요,
-          <br /> 백엔드 개발자{" "}
-          <span className="text-PRIMARY font-semibold">{information.name}</span>
-          입니다.
-        </h1>
-        <div className="flex gap-1">
-          {information.contact.map((contact) => (
-            <ContactItem
-              key={contact.id}
-              className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
-              {...contact}
-            >
-              {contact.name}
-            </ContactItem>
-          ))}
+        <div className="flex gap-4">
+          <img src="/images/information/0.png" alt="test" width={150} height={150}/>
+          <div className="grid grid-rows-auto gap-2">
+            <h1 className="leading-[1.15]">
+              {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
+              안녕하세요,<br/> 
+              백엔드 개발자{" "}<span className="text-PRIMARY font-semibold">{information.name}</span>입니다.
+            </h1>
+            <div className="flex gap-1">
+              {information.contact.map((contact) => (
+                <ContactItem
+                  key={contact.id}
+                  className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
+                  {...contact}
+                >
+                  {contact.name}
+                </ContactItem>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
       <Introduce markdown={information.markdown} />
