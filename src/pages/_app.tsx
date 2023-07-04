f12104878e4c3fd4c37fbf7389785f324b98b4f1
dhalsdl12 <dhalsdl12@gmail.com>
2023-07-04 22:00:25 +0900

    test

---
+++ src/pages/_app.tsx
@@ -0,0 +1,61 @@
+import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+
+/**
+ * @description SEO를 위해 본인의 정보로 수정해주세요.
+ */
+const DEFAULT_SEO = {
+  title: "권오민 | Back-End Dev",
+  description: "안녕하세요, 백엔드 개발자 권오민입니다.",
+  canonical: "https://www.naver.com/",
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: "https://www.naver.com/",
+    title: "권오민 | Back-End Dev",
+    site_name: "권오민 | Back-End Dev",
+    images: [
+      {
+        url: "/share.png",
+        width: 285,
+        height: 167,
+        alt: "권오민 | Back-End Dev",
+      },
+    ],
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: "application-name",
+      content: "권오민 | Back-End Dev",
+    },
+    {
+      name: "msapplication-tooltip",
+      content: "권오민 | Back-End Dev",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+  ],
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default App;
