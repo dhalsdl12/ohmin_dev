9d94fa434a2dbdca06561e44e17b1ecddceed3ed
dhalsdl12 <dhalsdl12@gmail.com>
2023-07-09 23:47:24 +0900

    personal project add

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -7,11 +7,22 @@ const Project = ({ project }: Pick<DataProps, "project">) => {
   return (
     <>
       <div>
-        <SectionTitle>Project</SectionTitle>
+        <SectionTitle>Team Project</SectionTitle>
         <div className="flex flex-col gap-24">
           {[...project]
             .reverse()
             .filter((project) => project.isTeam)
+            .map((project) => (
+              <ProjectItem key={project.id} {...project} />
+            ))}
+        </div>
+      </div>
+      <div>
+        <SectionTitle>Personal Project</SectionTitle>
+        <div className="flex flex-col gap-24">
+          {[...project]
+            .reverse()
+            .filter((project) => !project.isTeam)
             .map((project) => (
               <ProjectItem key={project.id} {...project} />
             ))}
